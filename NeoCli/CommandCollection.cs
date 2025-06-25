caa07bf7f7c53e9d0864ee26aef1dbf33f0fde35
Alexander Schwahl <alexander.schwahl@web.de>
2025-06-25 16:04:03 +0200

    Added convenience parser to CommandCollection

--- NeoCli/CommandCollection.cs
+++ NeoCli/CommandCollection.cs
@@ -16,23 +16,51 @@ namespace NeoCli
 		/// </summary>
 		public string Documentation { get; } = documentation;
 
-		/// <summary>
-		/// Determines whether the specified key exists in the parsed arguments.
-		/// </summary>
-		/// <param name="key">The key or alias to check.</param>
-		/// <returns><c>true</c> if the key exists; otherwise, <c>false</c>.</returns>
-		public bool Contains(object key) => Get(key) != null;
+		public bool Empty => _parsedArgs.Count == 0;
 
 		/// <summary>
 		/// Gets the <see cref="Command"/> associated with the specified key or alias.
 		/// </summary>
 		/// <param name="key">The key or alias.</param>
 		/// <returns>The <see cref="Command"/> if found; otherwise, <c>null</c>.</returns>
-		public Command? Get(object key)
+		public Command? this[object key]
 		{
-			string? keyStr = key.ToString();
-			if (keyStr == null) return null;
-			return _parsedArgs.FirstOrDefault(a => a.Key == keyStr || a.Alias == keyStr);
+			get
+			{
+				string? keyStr = key.ToString();
+				if (keyStr == null) return null;
+				return _parsedArgs.FirstOrDefault(a => a.Key == keyStr || a.Alias == keyStr);
+			}
+		}
+
+		/// <summary>
+		/// Determines whether the specified key exists in the parsed arguments.
+		/// </summary>
+		/// <param name="key">The key or alias to check.</param>
+		/// <returns><c>true</c> if the key exists; otherwise, <c>false</c>.</returns>
+		public bool Contains(object key) => this[key] != null;
+
+		/// <summary>
+		/// Tries to get the value associated with the specified key and convert it to the specified type.
+		/// </summary>
+		/// <typeparam name="T">The type to convert the value to. Must implement <see cref="IConvertible"/>.</typeparam>
+		/// <param name="key">The key or alias to look up.</param>
+		/// <param name="value">When this method returns, contains the converted value if the key exists and conversion succeeds; otherwise, the default value for type <typeparamref name="T"/>.</param>
+		/// <returns><c>true</c> if the value was found and converted successfully; otherwise, <c>false</c>.</returns>
+		public bool TryGet<T>(object key, out T value) where T : IConvertible
+		{
+			value = default!;
+			var cmd = this[key];
+			if (cmd?.Value is null) return false;
+			try
+			{
+				value = (T)Convert.ChangeType(cmd.Value, typeof(T));
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
 		}
 
 		/// <summary>Returns an enumerator that iterates through the collection.</summary>
